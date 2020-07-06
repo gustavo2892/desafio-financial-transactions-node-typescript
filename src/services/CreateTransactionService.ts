4e2907510756ab38d002addaeade31a6e7461100
gustavo2892 <gustavo2892@gmail.com>
2020-07-06 20:02:53 -0300

    Validate Total

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -14,6 +14,12 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You not have available balance');
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
